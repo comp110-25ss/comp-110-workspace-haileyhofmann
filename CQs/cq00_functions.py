e0bb760bfa3dd0a61a6fff1adad9e1437419ee81
haileyhofmann <hhof@unc.edu>
2025-06-27 19:04:09 -0400

    Finished Exercise 00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,15 @@
+"""Hailey Hofmann"""
+
+
+def mimic(message: str) -> str:
+    """Returns message back"""
+    return message
+
+
+def main() -> None:
+    """Main function to test mimic"""
+    print(mimic(message=input("What is your message")))
+
+
+if __name__ == "__main__":
+    main()
